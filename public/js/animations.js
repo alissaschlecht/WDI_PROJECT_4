856a38e69023f57a0e9df8d49d0f3e494b47c505
Alissa Schlecht <schlecht220@gmail.com>
2016-08-03 20:43:19 +0100

    added start screen

--- public/js/animations.js
+++ public/js/animations.js
@@ -5,30 +5,6 @@ angular.module('buildBikes')
 
 function AnimateController($scope, $timeout){
 
-  $scope.first = 'inline';
-  $scope.second = 'none';
-
-  $timeout(function() {
-    var fadeOut = document.getElementById('backgroundImage1');
-    var fadeOut2 = document.getElementById('firstLineText');
-
-    fadeOut.classList.add('animated');
-    fadeOut.classList.add('fadeOutLeftBig');
-    fadeOut2.classList.add('animated');
-    fadeOut2.classList.add('fadeOutLeftBig');
-
-    $scope.second = 'inline';
-   
-    var fadeIn = document.getElementById('backgroundImage2');
-    var fadeIn2 = document.getElementById('secondLineText');
-
-    fadeIn.classList.add('animated');
-    fadeIn.classList.add('fadeInRightBig');
-    fadeIn2.classList.add('animated');
-    fadeIn2.classList.add('fadeInRightBig');
-    
-
-  }, 5000);
   
 }
 
